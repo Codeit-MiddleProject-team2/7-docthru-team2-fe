a6dbde99a53469c49bff45b5e5ef163fca7eee8f
yewonlee1211 <yewonlee1211@korea.ac.kr>
2025-07-31 15:25:19 +0900

    랜딩페이지 임시 커밋

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
-import Head from "next/head";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      랜딩 페이지입니다.
+      <div>
+        <div>
+          <Image />{" "}
+        </div>
+      </div>
     </div>
   );
 }
