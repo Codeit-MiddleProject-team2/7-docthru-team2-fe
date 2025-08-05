67120fac9a11c446b446e0eadacae35ef5168c04
yewonlee1211 <yewonlee1211@korea.ac.kr>
2025-08-06 00:26:58 +0900

    분야 category 컴포넌트 완료 #25

--- components/challengeApply/category.js
+++ components/challengeApply/category.js
@@ -3,6 +3,22 @@ import { getCategory } from "@/mock/categoryMock";
 import styles from "./category.module.css";
 import Image from "next/image";
 
+function CategoryTag({ text, key, onPlus, onDelete, style = "plus" }) {
+  return (
+    <div className={styles.filteredCategory} key={key} onClick={onPlus}>
+      {text}
+      <Image
+        className={style === "plus" ? styles.plusBtn : styles.deleteBtn}
+        onClick={onDelete}
+        src={"/icons/ic_plus.svg"}
+        width={16}
+        height={16}
+        alt="분야 추가 혹은 삭제"
+      />
+    </div>
+  );
+}
+
 export default function Category({ category, setCategory }) {
   // value = 분야에 입력되고 있는 값, categorys = api에서 키워드 검색으로 불러온 데이터 목록, isLoading = 로딩 중에 화면 노출 x
   const [value, setValue] = useState("");
@@ -27,6 +43,12 @@ export default function Category({ category, setCategory }) {
     setCategory(value);
   };
 
+  // 선택한 분야 태그 삭제할 때
+  const handleCategoryDelete = () => {
+    setValue("");
+    setCategory("");
+  };
+
   useEffect(() => {
     // api 흉내로 키워드 검색 실행. 비동기. 검색어(value)가 바뀔 때마다 실행.
     const searchByValue = async () => {
@@ -47,25 +69,21 @@ export default function Category({ category, setCategory }) {
 
   console.log(category);
 
+  //상황이 1. 선택한 카테고리가 있을 때.
+  // 2. 선택한 카테고리가 없고, 입력했을 때 선택할 수 있는 검색 결과가 있을 때
+  // 3. 선택한 카테고리가 없고, 입력했을 때 선택할 수 있는 검색 결과가 없을 때
+
+  // 1번 상황
   if (category) {
     return (
       <div className={styles.category}>
         <label className={styles.text}>분야</label>
         <div className={styles.input}>
-          <div className={styles.filteredCategory}>
-            {category}
-            <Image
-              className={styles.deleteBtn}
-              onClick={() => {
-                setValue("");
-                setCategory("");
-              }}
-              src={"/icons/ic_plus.svg"}
-              width={16}
-              height={16}
-              alt="분야 삭제"
-            />
-          </div>
+          <CategoryTag
+            text={category}
+            onDelete={handleCategoryDelete}
+            style="delete"
+          />
         </div>
       </div>
     );
@@ -78,25 +96,20 @@ export default function Category({ category, setCategory }) {
       <div className={styles.searchResult}>
         <div className={styles.text}>추천 분야</div>
         <div className={styles.categoryList}>
-          {!isLoading &&
+          {(!isLoading &&
+            categorys.length &&
             categorys.map((category) => {
               return (
-                <div
-                  className={styles.filteredCategory}
-                  key={category.id}
-                  onClick={handleCategoryClick}
-                >
-                  {category.name}
-                  <Image
-                    className={styles.plusBtn}
-                    src={"/icons/ic_plus.svg"}
-                    width={16}
-                    height={16}
-                    alt="분야 추가"
-                  />
-                </div>
+                <CategoryTag
+                  text={category.name}
+                  key={category.key}
+                  onPlus={handleCategoryClick}
+                />
               );
-            })}
+            })) ||
+            (!categorys.length && (
+              <CategoryTag text={value} onPlus={handleCategoryClick} />
+            ))}
         </div>
       </div>
     </div>
