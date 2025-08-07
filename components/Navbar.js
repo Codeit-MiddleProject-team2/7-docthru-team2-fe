2acc56475ee322ffdfb22a70786d0755601e6fd0
yewonlee1211 <yewonlee1211@korea.ac.kr>
2025-08-07 15:31:53 +0900

    useAuth() 사용자 인증 관련 기초 작업 #9

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,33 @@
 import Image from "next/image";
 import styles from "./Navbar.module.css";
 import { useRouter } from "next/router";
+import { userLogin, userLogout, userSetting } from "@/lib/useAuth";
+import { useState, useEffect } from "react";
+import CustomBtnMini from "./CustomBtnMini";
+
+// 임의의 로그인, 로그아웃 버튼 navbar에 추가
+// 로그인 버튼을 클릭하면 다음과 같은 유저로 로그인 한 것과 같은 기능을 한다
+// id: test123, nickname: yewon, userLevel: "일반", isAdmin: false
+
+const exampleUser = {
+  user: { id: "test123", nickname: "yewon", userLevel: "일반", isAdmin: false },
+};
 
 export default function Navbar() {
   const router = useRouter();
+  const [isLogin, setIsLogin] = useState(false);
+  const [user, setUser] = useState({});
+
+  // 로그인 된 상태라면,
+  useEffect(() => {
+    try {
+      const { user: userData } = userSetting();
+      if (userData) {
+        setIsLogin(true);
+        setUser(userData);
+      }
+    } catch (e) {}
+  }, []);
 
   return (
     <div className={styles.background}>
@@ -22,20 +46,40 @@ export default function Navbar() {
           />
           <div className={styles.title}>Docthru</div>
         </div>
-        <div className={styles.user}>
-          <Image
-            src={"/icons/ic_bell.svg"}
-            width={24}
-            height={24}
-            alt="알림 아이콘"
+        {!isLogin && (
+          <CustomBtnMini
+            text={"로그인"}
+            onClick={() => {
+              userLogin(exampleUser);
+              setIsLogin(true);
+              router.push("/challenges");
+            }}
           />
-          <Image
-            src={"/icons/ic_profile.svg"}
-            width={32}
-            height={32}
-            alt="프로필 아이콘"
-          />
-        </div>
+        )}
+        {isLogin && (
+          <div className={styles.user}>
+            <Image
+              src={"/icons/ic_bell.svg"}
+              width={24}
+              height={24}
+              alt="알림 아이콘"
+            />
+            <Image
+              src={"/icons/ic_profile.svg"}
+              width={32}
+              height={32}
+              alt="프로필 아이콘"
+            />
+            <CustomBtnMini
+              text="로그아웃"
+              onClick={() => {
+                userLogout();
+                setIsLogin(false);
+                router.push("/");
+              }}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
