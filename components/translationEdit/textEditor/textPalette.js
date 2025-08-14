d65e41ac2ca324294c18c6f0278f9c1862ce124d
ehj <ehj9118@gmail.com>
2025-08-14 11:56:00 +0900

    네이밍 일치 및 주석으로 작업 시 안내, 유의사항 추가 #26

--- components/translationEdit/textEditor/textPalette.js
+++ components/translationEdit/textEditor/textPalette.js
@@ -1,3 +1,12 @@
+/* 편의성을 위해 텍스트 팔레트의 css 파일을 분리해두었습니다. 
+해당 컴포먼트는 컬러 팔레트 아이콘을 클릭할 시 나오게 되는 컴포먼트이며 
+유저가 색상을 선택할 시 닫히도록 되어있습니다. 
+기본적으로 색상은 검정, 회색, 빨강, 노랑, 녹색, 파랑, 보라 로 지정해두었으며. 
+번역 시 주석 혹은 강조 목적으로 쓰일 것으로 생각해 부러 많은 색상을 추가하지 않았습니다. 
+또한, 부러 원색의 경우 눈 피로도를 고려해 선택 색상의 채도를 낮추어 가능한 편의성을 고려했습니다. */
+
+import styles from './TextPalette.module.css';
+
 const COLORS = [
   '#000000', 
   '#6C757D', 
@@ -12,33 +21,16 @@ function TextPalette({ editor, onClose }) {
   if (!editor) return null;
 
   return (
-    <div
-      style={{
-      position: 'fixed',
-      zIndex: 9999, 
-      background: '#fff',
-      border: '1px solid',
-      padding: '8px',
-      display: 'flex',
-      gap: '4px',
-      width:'200px',
-      margin:'0px 20px',
-      }}
-    >
+    <div className={styles.palette}>
       {COLORS.map((color) => (
         <button
           key={color}
           onClick={() => {
             editor.chain().focus().setColor(color).run();
-            onClose?.(); 
+            onClose?.();
           }}
-          style={{
-            backgroundColor: color,
-            width: '24px',
-            height: '24px',
-            border: '1px solid #ccc',
-            cursor: 'pointer',
-          }}
+          className={styles.colorButton}
+          style={{ backgroundColor: color }}
         />
       ))}
     </div>
--- pages/translationEdit.js
+++ pages/translationEdit.js
@@ -1,28 +1,30 @@
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
-import Sidebar from '../components/translationEdit/originSidebar'
+import OriginSidebar from '../components/translationEdit/originSidebar'
 const EditorTool = dynamic(() => import('@/components/translationEdit/textEditor/EditorTool'), {
   ssr: false, 
 });
 
+/* 사이드바를 열고 닫는 함수를 변수에 저장*/
 function TranslationEditPage() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const openSidebar = () => {
-    setIsSidebarOpen(true);
+  const [isOriginSidebarOpen, setIsOriginSidebarOpen] = useState(false);
+  const openOriginSidebar = () => {
+    setIsOriginSidebarOpen(true);
   }
-  const closeSidebar = () => {
-    setIsSidebarOpen(false);
+  const closeOriginSidebar = () => {
+    setIsOriginSidebarOpen(false);
   };
   return (
     <>
      <div>
-        {/* 사이드바를 여는 버튼 */}
-        <button onClick={openSidebar}>
+        {/* 사이드바를 여는 버튼에 함수를 통해 사이드바를 열고 닫을 수 있도록 함 */}
+        <button onClick={openOriginSidebar}>
           원문 보기
         </button>
+        {/* 에디터툴 임포트(이미 만들어진 툴바 컴포먼트) */}
         <EditorTool/>
       </div>
-     <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
+     <OriginSidebar isOpen={isOriginSidebarOpen} onClose={closeOriginSidebar} />
     </>
   );
 }
