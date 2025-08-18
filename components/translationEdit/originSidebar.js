9d2f5d4b6e86141d3930fa9884e8f348de779742
dslee0525 <lds8835@gmail.com>
2025-08-18 22:37:57 +0900

    feat: 작업물 생성 페이지 ui 마크업 및 스타일 수정 #26

--- components/translationEdit/originSidebar.js
+++ components/translationEdit/originSidebar.js
@@ -1,14 +1,45 @@
-import styles from './originSidbar.module.css';
+import Image from "next/image";
+import styles from "./originSidbar.module.css";
+import Link from "next/link";
 
-const OriginSidebar = ({ isOpen, onClose }) => {
+const OriginSidebar = ({ isOpen, setIsOpenSidebar }) => {
   return (
-    <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
-      <button className={styles.closeButton} onClick={onClose}>
-        X
-      </button>
+    <div className={`${styles.sidebar} ${isOpen ? styles.opened : ""}`}>
       <nav>
-          <p>여기에 사이드바 아이프레임이 구현됩니다</p>
+        <button
+          className={styles.closeButton}
+          onClick={() => {
+            setIsOpenSidebar(false);
+          }}
+        >
+          <Image
+            src={"/icons/ic_close.svg"}
+            width={24}
+            height={24}
+            alt="닫기 버튼 아이콘"
+          />
+        </button>
+        <Link href={""} className={styles.linkButton}>
+          링크열기
+          <Image
+            src={"/icons/ic_out_link.svg"}
+            width={24}
+            height={24}
+            alt="외부링크 아이콘"
+          />
+        </Link>
       </nav>
+
+      <div className={styles.originArea}>
+        <iframe
+          src={"https://nextjs.org/docs/app/getting-started/layouts-and-pages"}
+          width="100%"
+          height="auto"
+          title="문서 미리보기"
+          tabIndex={-1}
+          scrolling="yes"
+        />
+      </div>
     </div>
   );
 };
